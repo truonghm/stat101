1b3527b13845c16cc81c62f0b37be867f02718e1
truonghm <truong173@outlook.com>
2020-11-18 23:18:50 +0700

    restart project!

--- bd_function.R
+++ bd_function.R
@@ -1,4 +1,8 @@
 unlink("docs", recursive=TRUE)
+if (file.exists("_main.Rmd")) 
+    #Delete file if it exists
+    file.remove("_main.Rmd")
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 file.create('docs/.nojekyll')
 rm(list = ls())
+rstudioapi::viewer("docs/index.html")
